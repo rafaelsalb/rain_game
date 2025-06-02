45db4322e20bb976eddf7f80683b22bb211fc9c8
Rafael Albuquerque <rafael.silva.albuquerque@hotmail.com>
2025-06-02 20:48:19 -0300

    cutscenes

--- scenes/effects/rain/rain.gd
+++ scenes/effects/rain/rain.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 
-@export var emission_offset: Vector2 = Vector2.ZERO;
+@export var emission_offset: Vector2 = Vector2.ZERO
+@export_range(0.0, 1.0) var audio_volume: float = 1.0
+@export_enum("Calm", "Strong") var rain_strength: int
+
+@onready var audio_stream_player_selector = $AudioStreamPlayerSelector
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var audio_stream = audio_stream_player_selector.get_children()[rain_strength]
+	audio_stream.volume_linear = audio_volume
+	audio_stream.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
